cf85e0cd5952d06e90bc753d71da7517d95131bc
Drew McElligott <rdmcelligott@gmail.com>
2016-10-25 17:26:34 -0500

    Corrects errors with some of the api integration functionality

--- server/controllers/giphyController.js
+++ server/controllers/giphyController.js
@@ -3,10 +3,10 @@ var axios = require('axios')
 module.exports = {
 
   gifGet: (req,res) => {
-    var keyword = req.body
+    var keyword = req.body.keyword
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=dc6zaTOxFJmzC`)
-      .then(function(res) {
-        return res.body[data][0][embed_url]
+      .then(function(result) {
+        res.send(result.data.data[0].embed_url)
       })
       .catch(function(err) {
         console.log("ERRRRRRRROOOORRRRRRRRR in server/controllers/giphyController.js")
