7384b6c52e6509f80346fcf722d000b36f1b94fa
Drew McElligott <rdmcelligott@gmail.com>
2016-10-25 14:27:23 -0500

    Removes standard css and adds less file structure and functionality to index.html, server, and front end files for future use

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         }
       },
 
-      { 
+      {
         test: /\.json$/,
         loader: 'json-loader'
       },
@@ -33,8 +33,8 @@ module.exports = {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       },
-      
-      { 
+
+      {
         test: /\.png$/,
         loader: "file-loader"
       }
