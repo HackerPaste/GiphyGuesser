0020f2329304dc7513d880dfb2b61b0abc688b3b
Alex Mitchell <shamsup@users.noreply.github.com>
2016-10-25 15:08:46 -0500

    Remove extra routes that were moved to server.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -13,32 +13,9 @@ isAuthed = (req,res,next) => {
 
 //Connect controller methods to their corresponding routes
 router.route('/stories').get(stories.getAllStories)
-
 router.route('/user').get(isAuthed, users.get)
-
 router.route('/stories/:id').get(isAuthed, stories.joinStory, stories.getOneStory)
-
 router.route('/stories').post(isAuthed,stories.createStory)
-
 router.route('/stories/:id').put(stories.createNewLine)
 
-router.route('/logout').get((req,res) => {
-  req.logout()
-  res.redirect('/')
-})
-
-router.route('/auth/facebook').get(passport.authenticate('facebook'))
-
-router.get('/', (req,res) => {
-  res.sendFile(path.resolve(__dirname, '../../dist/index.html'))
-})
-
-// facebook will call this URL
-router.route('/auth/facebook/return').get(passport.authenticate('facebook', {
-  failureRedirect: '/#/fail',
-  successRedirect: '/#/',
-}))
-router.route('/').get((req,res) => {
-  res.sendFile(path.resolve(__dirname, '../../dist/index.html'))
-})
 module.exports = router
