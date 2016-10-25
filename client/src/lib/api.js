7336aa750340a6286a4a01b7c17003da936b8102
Alex Mitchell <shamsup@users.noreply.github.com>
2016-10-25 15:51:51 -0500

    Replace jquery call with createStory function

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -5,3 +5,8 @@ API.getUser = function () {
   return http.get('/api/user')
     .then(res => res.data)
 }
+
+API.createStory = function (story) {
+  return http.post('/api/stories', story)
+    .then(res => res.data)
+}
