7f3f7db0bc9ba375cad856b6c85312b24f5d2bf4
Alex Mitchell <shamsup@users.noreply.github.com>
2016-10-27 14:05:30 -0500

    Add provisional methods

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,17 +1,63 @@
 var axios = require('axios')
+var giphy = axios.create({ baseURL: 'http://api.giphy.com/v1/gifs' });
 
-module.exports = {
+const games = module.exports
 
-  gifGet: (req,res) => {
-    var keyword = req.body.keyword
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=dc6zaTOxFJmzC`)
-      .then(function(result) {
-        res.send(result.data.data[0].embed_url)
-      })
-      .catch(function(err) {
-        console.log("ERRRRRRRROOOORRRRRRRRR in server/controllers/giphyController.js")
-      })
+games.fetchGiphy = function (keyword) {
+    return giphy.get(`/search?q=${keyword}&api_key=${process.env.GIPHY_KEY || 'dc6zaTOxFJmzC'}`)
+      .then(res => res.data)
+}
+
+games.create = function (topic, userId) {
+  if (!topic || !userId) {
+    return Promise.reject(new BadRequest('topic and userId are required'))
+  }
+  return new Game({ topic: topic, leader: userId })
+    .save().then(() => game)
+    .catch(err => new BadRequest('could not create game'))
+}
+
+games.find = function (id) {
+  if (!id) {
+    return Promise.reject(new BadRequest('gameId is required'))
   }
 
-  
-};
+  return Game.findOne({_id: id})
+    .then(game => {
+      if (!game) {
+        return new games.NotFound(id)
+      }
+      return game
+    })
+}
+
+games.joinGame = function (gameId, userId) {
+  return games.find(gameId)
+    .then(game => {
+      if (!game.users.includes(userId)) {
+        if (game.users.length < game.maxPlayers) {
+          game.users.push(userId)
+          return Game.update({_id: gameId}, { $push: { users: userId } })
+            .then(() => game)
+        }
+
+        return new games.BadRequest(`game ${gameId} is full`)
+      }
+    })
+}
+
+games.NotFound = class NotFound extends Error {
+  constructor(id) {
+    super()
+    this.statusCode = 404
+    this.details = { reason: `game ${id} not found` }
+  }
+}
+
+games.BadRequest = class BadRequest extends Error {
+  consctructor(reason) {
+    super()
+    this.statusCode = 400
+    this.details = { reason: reason }
+  }
+}
