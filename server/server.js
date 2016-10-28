efe7b68d90d12d41779722086c9d36d72aa32513
Alex Mitchell <shamsup@users.noreply.github.com>
2016-10-28 01:36:20 -0500

    Implement socketio authentication via passport

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ if (!process.env.DBPATH) {
 }
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const passport = require('passport')
 const FacebookStrategy = require('passport-facebook').Strategy
+const passportSocketIo = require('passport.socketio')
 const session = require('express-session')
+const sessionStore = require('connect-mongo')(session)
 const path = require('path')
 const morgan = require('morgan')
 const browserify = require('browserify-middleware')
@@ -13,13 +16,11 @@ const LESS = require('node-less-endpoint')
 
 const router = require('./routes/routes')
 const User = require('./models/user')
-const stories = require('./controllers/storyController')
+const db = require ('./models/config')
 const charles = require('./secretsecrets')
 
 const port = process.env.PORT || 4000
 const app = express()
-const server = require('http').Server(app);
-const io = require('./socket.js').listen(server);
 
 passport.serializeUser(function (user, done) {
   console.log(user)
@@ -66,9 +67,11 @@ passport.use(new FacebookStrategy({
   }
 ))
 
+const server = require('http').Server(app);
+const io = require('./socket').listen(server);
+
 // app level middleware
 app.use(morgan('dev'))
-
 app.get('/bundle.js', browserify('./client/index.js', {
   debug: true,
   transform: [
@@ -82,16 +85,29 @@ app.get('/style.css', LESS.serve('./client/less/index.less', {
 }));
 
 app.use(express.static(path.resolve(__dirname, '..client/public')))
+app.use(bodyParser.json())
 
-app.use(bodyParser.json());
+var mongoSession = new sessionStore({
+  mongooseConnection: db.connection
+})
 app.use(session({
   secret: charles.secret,
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: mongoSession
 }));
+
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// attach session middleware to socket io
+
+io.use(passportSocketIo.authorize({
+  cookieParser: cookieParser,
+  secret: charles.secret,
+  store: mongoSession
+}));
 
 app.get('/logout', (req,res) => {
   req.logout()
