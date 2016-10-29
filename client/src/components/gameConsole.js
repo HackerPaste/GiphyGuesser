90fb85d359a3797056325bb35b24cbf1e7783363
Drew McElligott <rdmcelligott@gmail.com>
2016-10-29 10:42:15 -0500

    Fixes small bug error

--- client/src/components/gameConsole.js
+++ client/src/components/gameConsole.js
@@ -8,7 +8,7 @@ module.exports = (props) => (
         <div>{props.gameOverFlag}The Winner is {props.gameOver.winner}!!!</div>
         <div>{props.gameOverFlag}The winning phrase was {props.gameOver.winningPhrase}</div>
 
-        <form className="subjectDisplay" onClick={props.handleKeywordSubmit}>
+        <form className="subjectDisplay" onSubmit={props.handleKeywordSubmit}>
           <input className="gameTextInput createTitleInput subjectinput" placeholder="Enter Giphy" type="text" name="guess" onChange={props.handleKeywordInput}/>
           <input type="submit" value="Submit" />
         </form>
@@ -16,4 +16,3 @@ module.exports = (props) => (
     </div>
   </div>
 )
-
