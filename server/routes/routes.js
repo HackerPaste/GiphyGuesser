95d7be6ce9a38cb131d30094b6e96db9433a1c78
Alex Mitchell <shamsup@users.noreply.github.com>
2016-10-29 09:57:53 -0500

    Correct game creation route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -18,21 +18,22 @@ router.get('/games', isAuthed, function (req, res) {
 })
 
 router.post('/games', isAuthed, function (req, res) {
-  games.create(req.body.topic, req.user._id)
+  console.log('create game req.body', req.body, req.user)
+  games.create(req.body.topic, req.user.facebookId)
     .then(respond(res, 201))
-    .catch(respond(res))
+    // .catch(respond(res))
 })
 
 router.get('/games/:gameId', function (req, res) {
   games.find(req.params.gameId)
     .then(respond(res))
-    .catch(respond(res))
+    // .catch(respond(res))
 })
 
 router.put('/games/:gameId', function (req, res) {
   games.joinGame(req.params.gameId, req.user._id)
     .then(respond(res))
-    .catch(respond(res))
+    // .catch(respond(res))
 })
 
 router.get('/user', isAuthed, function(req, res) {
@@ -41,12 +42,12 @@ router.get('/user', isAuthed, function(req, res) {
 
 
 function respond (res, status = 200) {
-  return function(data) {
-    if (data instanceof Error) {
-      res.status(data.status || 500)
+  return function(err) {
+    if (err instanceof Error) {
+      res.status(err.status || 500)
       return res.json(err.details)
     }
-    res.status(status).json(data)
+    res.status(status).json(err)
   }
 }
 
